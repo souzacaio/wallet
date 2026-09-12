05cfadef1fa9bbff9c3a72e48650097f0b86d193
souzacaio <41522937+souzacaio@users.noreply.github.com>
2026-09-12 16:13:53 -0300

    feat: add application entry point

---
+++ src/main/java/br/com/datum/wallet/WalletApplication.java
@@ -0,0 +1,12 @@
+package br.com.datum.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApplication.class, args);
+    }
+}
